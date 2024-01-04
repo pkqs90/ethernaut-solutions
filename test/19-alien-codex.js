48844aaf4b051e5a48e69578dcdb7ac4e8985dda
pkqs90 <pkqs90@gmail.com>
2024-01-04 10:24:07 +0800

    typo

--- test/19-alien-codex.js
+++ test/19-alien-codex.js
@@ -18,7 +18,7 @@ it("Solves 19-AlienCodex", async function () {
   await challenge.retract();
 
   // Array data is stored in keccak256(p), keccak256(p)+1, keccak256(p)+2, ... where p is the allocated slot
-  // in the order of variable declatarion (padded to 32 bytes).
+  // in the order of variable declararion (padded to 32 bytes).
   // https://docs.soliditylang.org/en/v0.8.23/internals/layout_in_storage.html#mappings-and-dynamic-arrays
   const startingPosition = BigInt(ethers.solidityPackedKeccak256(["uint256"], [1]));
   const delta = 2n**256n - startingPosition;
